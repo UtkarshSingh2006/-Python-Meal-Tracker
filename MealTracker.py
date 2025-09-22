c7ca35dd8bccdf2276ccabf42e7c8452015d28d9
UtkarshSingh2006 <singhfamily637@gmail.com>
2025-09-22 15:22:59 +0530

    Add files via upload

---
+++ MealTracker.py
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <body>
+        <form>
+            Name: <input type="text" placeholder="Enter your name" 
+            Email: <input type="email"
+            password: <input type="password"
+    </body>
+
+
+</html>
