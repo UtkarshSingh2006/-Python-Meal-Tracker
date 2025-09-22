9130b2e06b8f4e073696e5bd092c491d81f5bb94
UtkarshSingh2006 <singhfamily637@gmail.com>
2025-09-22 15:25:05 +0530

    MealTracker.py

--- MealTracker.py
+++ MealTracker.py
@@ -1,11 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-    <body>
-        <form>
-            Name: <input type="text" placeholder="Enter your name" 
-            Email: <input type="email"
-            password: <input type="password"
-    </body>
 
+print("this app tracks your routine.")
 
-</html>
+meals = []
+calorie = []
+# total_cl = 0
+daily_cl_lt = int(input("whats your daily limit of calorie : "))
+n = int(input("enter number of meals : "))
+
+for i in range(1,n+1):
+    mn = input("enter meal name : ")
+    cl = int(input("enter calorie : "))
+    # total_cl += cl
+    meals.append(mn)
+    calorie.append(cl)
+    
+sum_cl = sum(calorie)
+average_cl = sum_cl/n
+
+if daily_cl_lt > sum_cl :
+    print("your calorie intake is low than your limit.")
+elif daily_cl_lt < sum_cl :
+    print("Your daily limit of calorie is crossed now !")
+else:
+    print("calorie intake is equal right now")
+
+print("\nMeal Name\tCalories\n--------------------------------")
+
+for i in meals:
+    for j in calorie:
+        print(i,"\t",j)
+
+print(f"total:\t{sum_cl}\nAverage:\t{average_cl}")
